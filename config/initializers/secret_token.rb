ab1fdebaca6547fcd12d5a6d41cbc6b42d3ca895
Simon Taranto <simon.taranto@gmail.com>
2013-11-04 17:33:13 -0700

    initial commit with postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodfight::Application.config.secret_key_base = '45a4096f294b1709cf7593e76e3ab601fc30ae354e1e3b2a523074fe5a1e2072793a4a8b4222b68624860a4cfdf647b4dfb6e744fe43cee5056e6448e0f802ea'
