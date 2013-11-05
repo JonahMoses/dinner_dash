49a675cdfdae9a8263d02f72c6c7935e98ca710d
Jonah <Jonahkmoses@gmail.com>
2013-11-04 23:45:51 -0700

    added print_price to style price views

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,3 @@
 class Order < ActiveRecord::Base
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,9 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :item
+
+  def subtotal
+
+  end
+
 end
