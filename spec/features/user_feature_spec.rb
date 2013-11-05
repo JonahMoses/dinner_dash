119c06c1bc3dfe4679184c50eb1598f072fa4d23
Jonah <Jonahkmoses@gmail.com>
2013-11-05 10:05:11 -0700

    Removed duplicate flash notices on orders, order items, items

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -4,10 +4,10 @@ describe "the signup process", :type => :feature do
 
   it "signs me up" do
     visit '/sign_up'
-    fill_in 'user_email', :with => "user@example.com"
-    fill_in 'user_full_name', :with => "Bo Jangles"
-    fill_in 'user_display_name', :with => "BJ"
-    fill_in 'user_password', :with => "foobarbaz"
+    fill_in 'user_email',                 :with => "user@example.com"
+    fill_in 'user_full_name',             :with => "Bo Jangles"
+    fill_in 'user_display_name',          :with => "BJ"
+    fill_in 'user_password',              :with => "foobarbaz"
     fill_in 'user_password_confirmation', :with => "foobarbaz"
     click_link_or_button 'Create User'
     expect(page).to have_content 'Signed up!'
