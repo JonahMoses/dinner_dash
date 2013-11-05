d5cdc53c699430310d11f0acc5b5e8b7a85d54a4
Simon Taranto <simon.taranto@gmail.com>
2013-11-04 20:45:19 -0700

    update Item validations to meet spec
    update item#price validation to allow 8 digits on left of decimal
    
    add tests for empty strings on item title and description

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ActiveRecord::Base
   validates :title, :description, :price, presence: true
+  validates :title, uniqueness: true
+  validates :price, :numericality => { :greater_than => 0 }
+  validates_format_of :price, :with => /\A\d{1,8}(\.\d{2})?\z/
 end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Item do
+  it "forces unique title" do
+    fries = Item.create!(title: "Fries", description: "greasy sticks", price: 1.99)
+    fries_again = Item.create(title: "Fries", description: "slimy", price: 2.99)
+    fries_again.valid?.should be_false
+  end
+
+  it "has a price greater than 0" do
+    fries = Item.create(title: "Fries", description: "blah", price: -2.00)
+    fries.valid?.should be_false
+  end
+
+  it "has a decimal price" do
+    fries = Item.create(title: "Fries", description: "blah", price: 2)
+    fries.valid?.should be_false
+  end
+
+  it "rejects a price with only a tens digit" do
+    fries = Item.create(title: "Fries", description: "blah", price: 2.2)
+    fries.valid?.should be_false
+  end
+
+  it "rejects an empty title" do
+    fries = Item.create(title: "  ", description: "blah", price: 2.2)
+    fries.valid?.should be_false
+  end
+
+  it "rejects an empty descriptiont" do
+    fries = Item.create(title: "Fries", description: "  ", price: 2.2)
+    fries.valid?.should be_false
+  end
+
+end
