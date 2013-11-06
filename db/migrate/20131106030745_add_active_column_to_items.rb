989df189261746d9c33ad4daf355af75e293eb43
Jonah <Jonahkmoses@gmail.com>
2013-11-05 21:58:09 -0700

    Added active item boolean

---
+++ db/migrate/20131106030745_add_active_column_to_items.rb
@@ -0,0 +1,5 @@
+class AddActiveColumnToItems < ActiveRecord::Migration
+  def change
+    add_column :items, :active, :boolean, default: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131106004951) do
+ActiveRecord::Schema.define(version: 20131106030745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20131106004951) do
     t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "active",                              default: true
   end
 
   create_table "order_items", force: true do |t|
