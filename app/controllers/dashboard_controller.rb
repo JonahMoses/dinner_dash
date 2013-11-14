d96e0bfc8ca36101e19cc8d7585dff0242b88bd3
Simon Taranto <simon.taranto@gmail.com>
2013-11-13 19:23:25 -0700

    add order count by status

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def index
     @open_orders = Order.where(:status => "unsubmitted")
     @closed_orders = Order.where.not(:status => "unsubmitted")
+    @orders_by_status = Order.count(:group => "status")
   end
 end
