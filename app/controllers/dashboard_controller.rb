b09f4c7baa6087cc84155be79e2e1c787a157de6
Simon Taranto <simon.taranto@gmail.com>
2013-11-13 21:44:36 -0700

    implement filtering on orders dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,10 @@
 class DashboardController < ApplicationController
   def index
-    @open_orders = Order.where(:status => "unsubmitted")
-    @closed_orders = Order.where.not(:status => "unsubmitted")
     @orders_by_status = Order.count(:group => "status")
+    if params[:status]
+      @filtered_orders = Order.where(:status => params[:status])
+    else
+      @filtered_orders = Order.order("created_at DESC")
+    end
   end
 end
