a5541bc169f0be97831a92d2600fcae7d67d3a14
Simon Taranto <simon.taranto@gmail.com>
2013-11-13 19:38:27 -0700

    update order count to be in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :order_items_count
 
   before_action :authorize
 
@@ -8,6 +8,15 @@ class ApplicationController < ActionController::Base
   helper_method :allow?
 
   private
+
+    def order_items_count
+      if current_user
+        current_cart = Order.find_unsubmitted_order_for(current_user.id)
+        current_cart.items_count if current_cart
+      else
+        0
+      end
+    end
 
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
